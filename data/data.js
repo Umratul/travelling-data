992b5b1cc4e41d868c5fc261f72163f1499536b6
Umratul <umratul0904@gmail.com>
2021-11-06 23:25:08 +0600

    first commit

---
+++ data/data.js
@@ -0,0 +1,52 @@
+const details = [
+    {
+        id: 01,
+        name: 'Italy',
+        img: 'https://image.freepik.com/free-photo/beautiful-aerial-shot-florence-italy-architecture-evening_181624-2203.jpg',
+        duration: '3 Days 2 Nights',
+        price: 78.00,
+        body: "lorem10"
+    },
+
+    {
+        id: 02,
+        name: 'Paris',
+        img: 'https://image.freepik.com/free-photo/famous-eiffel-tower-paris-with-gorgeous-colors_268835-830.jpg',
+        duration: '5 Days 4 Nights',
+        price: 95.00,
+        body: ''  
+    },
+    {
+        id: 03,
+        name: 'Cuba',
+        img: 'https://image.freepik.com/free-photo/authentic-view-street-old-havana-with-old-buildings-cars_109800-8727.jpg',
+        duration: '5 Days 4 Nights',
+        price: 87.00,
+        body: ''  
+    },
+    {
+        id: 04,
+        name: 'Canada',
+        img: 'https://image.freepik.com/free-photo/vancouver-science-world_198523-43.jpg',
+        duration: '7 Days 6 Nights',
+        price: 104.00,
+        body: ''  
+    },
+    {
+        id: 05,
+        name: 'Brazil',
+        img: 'https://image.freepik.com/free-photo/vertical-shot-building-with-colonial-architecture-sao-luis-brazil_181624-44427.jpg',
+        duration: '10 Days',
+        price: 170.00,
+        body: ''  
+    },
+    {
+        id: 06,
+        name: 'Turkey',
+        img: 'https://image.freepik.com/free-photo/istanbul-s-ocean-with-cruise-ship_53876-42289.jpg',
+        duration: '3 Days 2 Nights',
+        price: 65.00,
+        body: ''  
+    }
+    
+]
